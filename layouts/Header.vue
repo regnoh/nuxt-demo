7e5ba3b3cd4cf2c626b118d3c8fa595cd35c0bd0
regnoh <1716532193@qq.com>
2019-12-19 13:58:22 +0800

    【布局Header: 顶部导航】 1. 展开、折叠 - 媒体查询 2. flex 3. button size round

--- layouts/Header.vue
+++ layouts/Header.vue
@@ -1,31 +1,78 @@
 <template>
   <div class="top-nav-container" ref="mobileNavRef">
-    <div class="nav-logo cursor">
-      <i class="iconfont icon-annotation_help"/>
+    <!-- logo -->
+    <div class="nav-logo">
+      <i class="iconfont icon-logowenzi" />
+    </div>
+    <!-- 展开的导航： pc -->
+    <div class="nav-menu-expand">
+      <!-- 左侧 -->
+      <div class="nav-left">ads</div>
+      <!-- 右侧 -->
+      <div class="nav-right">
+         <yl-button type="primary" round size="medium">免费注册</yl-button>
+      </div>
+    </div>
+    <!-- 折叠的导航： mobile -->
+    <div class="nav-menu-collapse">
+      <!-- 左侧注册按钮 -->
+      <yl-button type="primary" size="large">免费注册</yl-button>
+      <!-- 右侧折叠菜单icon -->
+      <i class="iconfont icon-list"></i>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'Header',
-}
+  name: "Header"
+};
 </script>
 <style lang='scss'>
-  .top-nav-container {
-    height: 64px;
-    background: #fff;
-    .nav-logo {
-      flex: none;
-      display: inline-block;
-      margin-right: 80px;
-
-      .iconfont {
-        font-size: 42px;
-        color: $--color-primary;
-      }
-    }
-    .cursor {
-      cursor: pointer;
+.top-nav-container {
+  height: 64px;
+  background: #fff;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  .nav-logo {
+    margin-right: 80px;
+    .iconfont {
+      font-size: 42px;
+      color: $--color-primary;
     }
   }
+  .nav-menu-expand {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    .nav-left {
+      width: 500px;
+      background: red;
+    }
+    .nav-right {
+      width: 300px;
+      background: green;
+    }
+  }
+  .nav-menu-collapse {
+    display: none;
+  }
+}
+@media screen and (max-width: 768px) {
+  .top-nav-container {
+    .nav-menu-expand {
+      display: none;
+    }
+    .nav-menu-collapse {
+      width: 150px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      .icon-list {
+        font-size: 30px;
+      }
+    }
+  }
+}
 </style>
